975271fc0cfa723b8d95d220e605f8ab2067f37d
DESKTOP-H6HE5VH\TA <reut@t-a9.com>
2021-06-27 22:56:34 +0300

    Add data grid table and content

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import axios from 'axios';
+import axios from "axios";
+import { DataGrid } from "@material-ui/data-grid";
 
-export default class App extends React.Component {
-  state = {
-    spans: []
-  }
+export const App = () => {
+  const [spans, setSpans] = useState([]);
 
-  componentDidMount() {
-    axios.get(`http://127.0.0.1:8000/spans`)
-      .then(res => {
-        console.log(res)
-        const spans = res.data.data;
-        this.setState({ spans });
-      }).catch((error)=>{
-        console.error(error)
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:8000/spans`)
+      .then((response) => {
+        console.log(response);
+        const spansData = response.data.map((span) => {
+          return {
+            ...span,
+            id: span.spanId,
+          };
+        });
+        setSpans(spansData);
       })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const columns = [
+    { field: "spanId", headerName: "Span Id", width: 200, sortable: true },
+    {
+      field: "operationName",
+      headerName: "Operation Name",
+      width: 350,
+      sortable: true,
+    },
+    {
+      field: "duration",
+      headerName: "Duration",
+      width: 350,
+      type: "number",
+      sortable: true,
+    },
+  ];
+
+  if (!spans || spans.length === 0) {
+    return <h1>No spans</h1>;
   }
 
-  render() {
-    const {spans} = this.state
-    if (!spans){
-      return <h1>No spans</h1>
-    }
-    return (
-      <ul>
-        { spans.map(span => <li>{JSON.stringify(span)}</li>)}
-      </ul>
-    )
-  }
-}
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={spans} columns={columns} />;
+    </div>
+  );
+};
